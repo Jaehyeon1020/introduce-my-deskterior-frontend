a8c5faa11685c43f43afcce397cde1a375fbf4b5
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-26 01:45:50 +0900

    feat: 글 삭제 기능 구현

--- src/routes/Board.js
+++ src/routes/Board.js
@@ -5,7 +5,7 @@
 import { sections } from "../lib/sections";
 import { useEffect, useState } from "react";
 import { loginCheck } from "../lib/loginCheck";
-import { CssBaseline, Grid, Typography } from "@mui/material";
+import { ButtonGroup, CssBaseline, Grid, Typography } from "@mui/material";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Container from "@mui/material/Container";
@@ -13,13 +13,14 @@ import MainFeaturedPost from "../components/MainFeaturedPost";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { getMainFeaturedPost } from "../lib/getMainFeaturedPost";
 import { getBoardTypeFromUrl } from "../lib/getBoardTypeFromUrl";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import Button from "@mui/material/Button";
 import axios from "axios";
-import { width } from "@mui/system";
 
 const theme = createTheme();
 
 export default function Board(props) {
+  const navigate = useNavigate();
   const { id } = useParams();
 
   const [loginStatus, setLoginStatus] = useState(false);
@@ -27,6 +28,24 @@ export default function Board(props) {
 
   const boardType = getBoardTypeFromUrl(window.location.href);
   const mainFeaturedPost = getMainFeaturedPost(boardType);
+
+  /* 글 수정 버튼 */
+  const patchButtonHandler = function () {
+    // 글 수정 페이지로 연결
+  };
+
+  /* 글 삭제 버튼 */
+  const deleteButtonHandler = function () {
+    axios
+      .delete(boardType + "/" + id)
+      .then((res) => {
+        alert("글이 삭제되었습니다.");
+        navigate("/" + boardType);
+      })
+      .catch((err) => {
+        alert("글을 삭제할 수 없습니다.");
+      });
+  };
 
   useEffect(() => {
     axios
@@ -54,23 +73,33 @@ export default function Board(props) {
         />
         <MainFeaturedPost post={mainFeaturedPost} />
         <main>
-          <Grid container spacing={0} paddingLeft="20px" marginBottom="30px">
-            <Typography className="board-title" variant="h3" fontSize="35px">
+          <Grid
+            container
+            paddingLeft="45px"
+            marginBottom="30px"
+            display="block"
+          >
+            <Typography className="board-title" variant="h3" fontSize="30px">
               제목: {boardContent.title}
             </Typography>
+            <Typography align="right">
+              작성자: {boardContent.authorName}
+            </Typography>
+            <Typography align="right">
+              작성일: {boardContent.createdAt}
+            </Typography>
           </Grid>
-          <Grid container spacing={0} paddingLeft="20px">
+          <Grid container paddingLeft="45px" marginBottom="50px">
             <img
               src={boardContent.image}
-              alt="image of this post"
+              alt="post"
               style={{
                 width: "65%",
                 height: "auto",
-                marginBottom: "15px",
               }}
             />
           </Grid>
-          <Grid container spacing={0} paddingLeft="20px">
+          <Grid container paddingLeft="45px">
             <Typography
               className="board-description"
               variant="body1"
@@ -78,6 +107,23 @@ export default function Board(props) {
             >
               {boardContent.description}
             </Typography>
+          </Grid>
+          <Grid container paddingLeft="45px">
+            <Grid xs={11}></Grid>
+            <Grid>
+              <ButtonGroup>
+                <Button variant="text" onClick={patchButtonHandler}>
+                  수정
+                </Button>
+                <Button
+                  variant="text"
+                  color="error"
+                  onClick={deleteButtonHandler}
+                >
+                  삭제
+                </Button>
+              </ButtonGroup>
+            </Grid>
           </Grid>
         </main>
       </Container>
