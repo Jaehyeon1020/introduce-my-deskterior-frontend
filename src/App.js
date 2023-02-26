a8e8345d3848b3270ee8128f665ceae1c804ac42
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-26 14:08:48 +0900

    feat: 새 글 작성 경로 라우터 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HoneyItemBoards from "./routes/HoneyItemBoards";
 import QuestionBoards from "./routes/QuestionBoards";
 import axios from "axios";
 import Board from "./routes/Board";
+import NewBoard from "./routes/NewBoard";
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.withCredentials = true;
@@ -15,14 +16,36 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/deskteriors" />} />
+
+        {/* 데스크테리어 게시판 */}
         <Route path="/deskteriors" element={<DeskteriorBoards />} />
         <Route path="/deskteriors/:id" element={<Board />} />
+        <Route
+          path="/deskteriors/new"
+          element={<NewBoard boardType="deskteriors" />}
+        />
+
+        {/* 꿀템 추천 게시판 */}
         <Route path="/honeyitems" element={<HoneyItemBoards />} />
         <Route path="/honeyitems/:id" element={<Board />} />
+        <Route
+          path="/honeyitems/new"
+          element={<NewBoard boardType="honeyitems" />}
+        />
+
+        {/* 질문 게시판 */}
         <Route path="/questions" element={<QuestionBoards />} />
         <Route path="/questions/:id" element={<Board />} />
+        <Route
+          path="/questions/new"
+          element={<NewBoard boardType="questions" />}
+        />
+
+        {/* 로그인, 회원가입 */}
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+
+        {/* 유저 정보 */}
         <Route path="/user/:id" element={<h1>유저 정보 페이지</h1>} />
       </Routes>
     </BrowserRouter>
