2cfc0515d9265b7aa93cdf3497b1526d8b9dd0cc
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-27 22:30:19 +0900

    refactor: 어느 위치에서 클릭해도 올바른 글로 갈 수 있도록 절대주소로 링크 수정

--- src/components/FeaturedPost.js
+++ src/components/FeaturedPost.js
@@ -8,11 +8,11 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
 function FeaturedPost(props) {
-  const { post } = props;
+  const { post, boardType } = props;
 
   return (
     <Grid item xs={12} md={6}>
-      <CardActionArea component="a" href={window.location.href + "/" + post.id}>
+      <CardActionArea component="a" href={"/" + boardType + "/" + post.id}>
         <Card sx={{ display: "flex" }}>
           <CardContent sx={{ flex: 1 }}>
             <Typography component="h2" variant="h5">
