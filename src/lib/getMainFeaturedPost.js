6a38691caf0e6b3cf8216ae248c8fa202d2949b2
Jaehyeon1020 <kjhkk1020@naver.com>
2023-02-27 22:31:22 +0900

    feat: 카드 타입에 메인페이지 추가

--- src/lib/getMainFeaturedPost.js
+++ src/lib/getMainFeaturedPost.js
@@ -21,12 +21,20 @@ export function getMainFeaturedPost(boardType) {
       imageText: "main image description",
       linkText: "",
     };
-  } else {
+  } else if (boardType === "questions") {
     return {
       title: "질문하기",
       description: "이거 뭐에요? 궁금했던 제품을 물어보세요!",
       image:
         "https://images.unsplash.com/photo-1484069560501-87d72b0c3669?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80",
+      imageText: "main image description",
+      linkText: "",
+    };
+  } else {
+    return {
+      title: "마이페이지",
+      image:
+        "https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2375&q=80",
       imageText: "main image description",
       linkText: "",
     };
